5d3051fede88cb456e14f42d0c5a8b94eb1236e7
magnsskj <magnsskj@stud.ntnu.no>
2021-02-03 13:05:21 +0100

    basic react setup med forside

---
+++ frontend/react-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Login />
+      <Info />    
+    </div>
+  );
+}
+
+export default App;
