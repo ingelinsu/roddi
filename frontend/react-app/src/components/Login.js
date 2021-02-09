d50d146d613a00503ebd77656f439b280ff8747a
amw <ase.wilhelmsen@outlook.com>
2021-02-09 17:38:05 +0100

    Fikset litt på forsiden

--- frontend/react-app/src/components/Login.js
+++ frontend/react-app/src/components/Login.js
@@ -1,12 +1,26 @@
-
-
-
+import React from 'react';
 
 
 function Login() {
     return (
-      <div>
-        {/*<Link to="/login">Login</Link>*/}
-      </div>
+      <div className="login-wrapper">
+      <h4>Logg inn:</h4>
+      <form>
+        <label>
+          <p>Username</p>
+          <input type="text" />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" />
+        </label>
+        <div>
+          <button id="knapp" type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
       );
   }
+
+
+  export default Login
