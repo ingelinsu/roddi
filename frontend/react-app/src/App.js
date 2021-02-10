2dc73245795d0e8098b851783f68811169077e5a
magnsskj <magnsskj@stud.ntnu.no>
2021-02-10 12:16:24 +0100

    Lagt til header og footer for alle sider i App.js

--- frontend/react-app/src/App.js
+++ frontend/react-app/src/App.js
@@ -11,13 +11,20 @@ import {
 import MainPage from "./pages/index.jsx";
 import LoginPage from "./pages/login.jsx";
 
+import Navbar from "./components/Navbar.js";
+import Footer from "./components/Footer.js";
+
 class App extends Component {
   render() {
     return (
-      <Router>
-       <Route exact path="/" component={MainPage} />
-       <Route exact path="/login" component={LoginPage} />
-      </Router>
+      <div>
+        <Navbar />
+        <Router>
+          <Route exact path="/" component={MainPage} />
+          <Route exact path="/login" component={LoginPage} />
+        </Router>
+        <Footer />
+      </div>
     );
   }
 }
