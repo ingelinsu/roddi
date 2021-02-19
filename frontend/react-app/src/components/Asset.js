83f8738bb555908201d1088cc6b3f0f3e2de6d28
magnsskj <magnsskj@stud.ntnu.no>
2021-02-19 10:46:11 +0100

    Endret visning av assets med css

--- frontend/react-app/src/components/Asset.js
+++ frontend/react-app/src/components/Asset.js
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
 
+import './Asset.css'
+
 import Decisions from './Decisions.js'
 
 class Asset extends Component {
     render() {
         return (
-            <div>
-                <h1 className="asset">{this.props.name}</h1>
-                <p>{this.props.description}</p>
+            <div className="asset">
+                <div className="assetImage">
+                    <img src={this.props.imgUrl} style={{height:150, width:150}} />
+                </div>
+                
+                <div className="assetContent">
+                    <p className="assetName">{this.props.name}</p>
+                    <p className="assetCategory">Møbler</p>
+                    <p className="assetDescription">{this.props.description}</p>
+                </div>
+
                 <div className="buttons">
                     <Decisions />
                 </div>
