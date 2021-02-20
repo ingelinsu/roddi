451bf2c533e9cae1e2d73630e1d6dd29a5fc74f2
magnsskj <magnsskj@stud.ntnu.no>
2021-02-20 12:14:09 +0100

    endret bruken av props for Asset.js

--- frontend/react-app/src/components/Asset.js
+++ frontend/react-app/src/components/Asset.js
@@ -9,12 +9,12 @@ class Asset extends Component {
         return (
             <div className="asset">
                 <div className="assetImage">
-                    <img src={this.props.imgUrl} style={{height:150, width:150}} />
+                    <img src={this.props.image_url} style={{height:150, width:150}} />
                 </div>
                 
                 <div className="assetContent">
                     <p className="assetName">{this.props.name}</p>
-                    <p className="assetCategory">Møbler</p>
+                    <p className="assetCategory">{this.props.category}</p>
                     <p className="assetDescription">{this.props.description}</p>
                 </div>
 
