68102163c86cda3008f90c53a9be5a449b0b2212
amw <ase.wilhelmsen@outlook.com>
2021-02-21 17:23:12 +0100

    Endret på Estate.js

--- frontend/react-app/src/components/Estate.js
+++ frontend/react-app/src/components/Estate.js
@@ -3,9 +3,14 @@ import React, {Component} from 'react';
 class Estate extends Component {
     render() {
         return (
-            <div>
-                <h1>Dodsbo</h1>
-                <h2>Adresse</h2>
+            <div className="estate">
+                <div className="estateImage">
+                    <img src={this.props.image_url} style={{height:150, width:150}} />
+                </div>
+                
+                <div className="estateContent">
+                    <p className="estateName">{this.props.name}</p>
+                </div>
             </div>
         );
 
