2b8652e0213fb0667713802041d2d5ca9fdcfc0d
amw <ase.wilhelmsen@outlook.com>
2021-02-21 17:18:11 +0100

    Endret på estates.jsx

--- frontend/react-app/src/pages/estates.jsx
+++ frontend/react-app/src/pages/estates.jsx
@@ -2,35 +2,31 @@ import React, {Component} from 'react';
 import axios from 'axios';
 
 import Estate from "../components/Estate.js"
-import Asset from "../components/Asset"
 
 class EstatePage extends Component {
 
-    // constructor(props) {
-    //     super(props)
-    //     this.state = {
-    //         data: []
-    //     }
-    // }
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: []
+        }
+    }
 
-    // componentDidMount() {
-    //     axios
-    //     .get("http://localhost:8000/api/assets/")
-    //     .then(response => this.setState({data: response.data}))
-    //     .catch(err => console.log(err))
-    // }
+    componentDidMount() {
+        axios
+        .get("http://localhost:8000/api/estates/")
+        .then(response => this.setState({data: response.data}))
+        .catch(err => console.log(err))
+    }
 
-    // mapResponseData() {
-    //     return this.state.data.map(asset => <Asset key={asset.key} name={asset.name} description={asset.description} />)
-    // }
-
-
+    mapResponseData() {
+        return this.state.data.map(estate => <Estate key={estate.key} name={estate.name} description={estate.description} />)
+    }
 
     render() {
         return (
             <div>
-                {/* {this.mapResponseData()} */}
-                <Estate />
+                {this.mapResponseData()}
             </div>
         );
     }
