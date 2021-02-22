bb42b808fab52b1fcb13b5092935b24a4deedb96
Daniel Faaravik Schiøtz <daniel.faaravik.schiotz@norconsult.com>
2021-02-22 14:26:57 +0100

    Fix login bug

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -80,7 +80,9 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny']
 }
 
 
--- backend/roddi/views.py
+++ backend/roddi/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import *
 from .models import *
 
@@ -26,8 +26,6 @@ class CommentView(viewsets.ModelViewSet):
 
 
 @api_view(['GET'])
-@authentication_classes((SessionAuthentication, BasicAuthentication, TokenAuthentication))
-@permission_classes((IsAuthenticated,))
 def login(request, email, password):
     users = User.objects.all()
     matching_users = [u for u in users if u.email==email and u.password==password]
