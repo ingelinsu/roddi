b51521a6a73b939566cfd8dacf246a8184f4c65d
magnsskj <magnsskj@stud.ntnu.no>
2021-02-24 17:17:19 +0100

    Link på hver dødsbo med litt style fix

--- frontend/react-app/src/components/Estate.js
+++ frontend/react-app/src/components/Estate.js
@@ -2,10 +2,18 @@ import React, {Component} from 'react';
 import './Estate.css'
 import home_icon from '../images/home_icon.png';
 
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Link,
+    Redirect
+  } from "react-router-dom";
 
 class Estate extends Component {
     render() {
         return (
+            <Link to="/eiendeler" className="assetLink">
             <div className="estate">
                 <div className="estateImage">
                     <img src= {home_icon}  /*{this.props.image_url}*/ style={{height:150, width:150}} />
@@ -16,6 +24,7 @@ class Estate extends Component {
                     <p className = "estateDescription">{this.props.description}</p>
                 </div>
             </div>
+            </Link>
         );
 
     }
