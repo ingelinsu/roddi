6b26a82a7092e14a4e6f86336cff1d9328a99e38
Daniel Faaravik Schiøtz <daniel.faaravik.schiotz@norconsult.com>
2021-03-15 11:08:35 +0100

    Add API function for approval

--- backend/roddi/views.py
+++ backend/roddi/views.py
@@ -12,30 +12,30 @@ import json
 
 
 class AssetView(viewsets.ModelViewSet):
-  serializer_class = AssetSerializer
-  queryset = Asset.objects.all()
+    serializer_class = AssetSerializer
+    queryset = Asset.objects.all()
 
 class EstateAssetsView(viewsets.ModelViewSet):
-  # JSON view of all the assets in a given estate.
-  serializer_class = EstateAssetsSerializer
-  queryset = Estate.objects.all()
+    # JSON view of all the assets in a given estate.
+    serializer_class = EstateAssetsSerializer
+    queryset = Estate.objects.all()
 
 class UserView(viewsets.ModelViewSet):
-  serializer_class = UserSerializer
-  queryset = User.objects.all()
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
 
 class UserEstatesView(viewsets.ModelViewSet):
-  # JSON view for all the estates associated with a user.
-  serializer_class = UserEstatesSerializer
-  queryset = User.objects.all()
+    # JSON view for all the estates associated with a user.
+    serializer_class = UserEstatesSerializer
+    queryset = User.objects.all()
 
 class EstateView(viewsets.ModelViewSet):
-  serializer_class = EstateSerializer
-  queryset = Estate.objects.all()
+    serializer_class = EstateSerializer
+    queryset = Estate.objects.all()
 
 class CommentView(viewsets.ModelViewSet):
-  serializer_class = CommentSerializer
-  queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
 
 
 @api_view(['GET'])
@@ -43,9 +43,9 @@ def login_view(request, email, password):
     users = User.objects.all()
     matching_users = [u for u in users if u.email==email and u.password==password]
     if len(matching_users) > 0:
-      json_response = {'id': matching_users[0].id}
+        json_response = {'id': matching_users[0].id}
     else:
-      json_response = {'denied': 'wrong credentials'}
+        json_response = {'denied': 'wrong credentials'}
     return Response(json_response)
 
 @api_view(['GET'])
@@ -69,17 +69,22 @@ def reprioritize_view(request, user_id, asset_id, new_prio):
 
 
 def vote_view(request, user_id, asset_id, vote):
-  if User.objects.filter(id=user_id).exists():
-    user = User.objects.get(id=user_id)
-  else:
-    return HttpResponse('User ID not valid', status=400) # bad request
-  if Asset.objects.filter(id=asset_id).exists():
-    asset = Asset.objects.get(id=asset_id)
-  else:
-    return HttpResponse('Asset ID not valid', status=400) # bad request
-
-  asset.vote(user, vote)
-  return HttpResponse(status=204) # no content
+    """
+    Set or change users vote for how to handle an asset.
+    user_id    INT id for the user
+    asset_id   INT id for the asset
+    vote       STR the vote. Must be "distribute", "throw", "donate" (or "withdraw" for withdrawing vote)
+    """
+    if User.objects.filter(id=user_id).exists():
+        user = User.objects.get(id=user_id)
+    else:
+        return HttpResponse('User ID not valid', status=400) # bad request
+    if Asset.objects.filter(id=asset_id).exists():
+        asset = Asset.objects.get(id=asset_id)
+    else:
+        return HttpResponse('Asset ID not valid', status=400) # bad request
+    asset.vote(user, vote)
+    return HttpResponse(status=204) # no content
 
 def register_view(request, name, pw, age, email):
     """
@@ -91,4 +96,22 @@ def register_view(request, name, pw, age, email):
     """
     user = User(name=name, password=pw, age=age, email=email)
     user.save()
+    return HttpResponse(status=204) # no content
+
+def approve_view(request, user_id, estate_id):
+    """
+    Approve an estate, i. e. vote for starting distribution
+    user_id    INT id for the user
+    estate_id  INT id for the estate
+    """
+    if User.objects.filter(id=user_id).exists():
+        user = User.objects.get(id=user_id)
+    else:
+        return HttpResponse('User ID not valid', status=400) # bad request
+    if Estate.objects.filter(id=estate_id).exists():
+        estate = Estate.objects.get(id=asset_id)
+    else:
+        return HttpResponse('Asset ID not valid', status=400) # bad request
+    estate.approve(user)
+    estate.save()
     return HttpResponse(status=204) # no content
