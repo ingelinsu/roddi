6b13de0603fd7eb3da4d4e8c5b3aff8dd9fcee4c
magnsskj <magnsskj@stud.ntnu.no>
2021-03-17 17:30:09 +0100

    prioritetsknapper m. metoder + henting av vote fra Decisions.js

--- frontend/react-app/src/components/Asset.js
+++ frontend/react-app/src/components/Asset.js
@@ -1,25 +1,47 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import './Asset.css'
 
 import Decisions from './Decisions.js'
 
 class Asset extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            decision: ""
+        }
+        this.getDecision = this.getDecision.bind(this)
+    }
+
+    onReprioritize(movement) {
+        this.props.onReorder(this.props.id, movement)
+    }
+
+    getDecision(vote) {
+        this.setState({ decision: vote }, () => this.props.onDecision(this.state.decision))
+    }
+
     render() {
         return (
-            <div className="asset">
-                <div className="assetImage">
-                    <img src={this.props.image_url} style={{height:150, width:150}} />
+            <div className="assetWrapper">
+                <div className="priority">
+                    <button type="button" onClick={(e) => this.onReprioritize("up")}>Up</button>
+                    <button type="button" onClick={(e) => this.onReprioritize("down")}>Down</button>
                 </div>
-                
-                <div className="assetContent">
-                    <p className="assetName">{this.props.name}</p>
-                    <p className="assetCategory">{this.props.category}</p>
-                    <p className="assetDescription">{this.props.description}</p>
-                </div>
+                <div className="asset">
+                    <div className="assetImage">
+                        <img src={this.props.image_url} style={{ height: 150, width: 150 }} />
+                    </div>
 
-                <div className="buttons">
-                    <Decisions assetId={this.props.id}/>
+                    <div className="assetContent">
+                        <p className="assetName">{this.props.name}</p>
+                        <p className="assetCategory">{this.props.category}</p>
+                        <p className="assetDescription">{this.props.description}</p>
+                    </div>
+
+                    <div className="buttons">
+                        <Decisions assetId={this.props.id} onVote={this.getDecision} />
+                    </div>
                 </div>
             </div>
         );
