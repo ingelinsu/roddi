03c3f72840bb9b37d5dabbfcf11e870fc8b016a3
magnsskj <magnsskj@stud.ntnu.no>
2021-03-23 18:23:26 +0100

    Bruker isNaN hvor 0 kan oppstå i if statement

--- frontend/react-app/src/components/PrivateRoute.js
+++ frontend/react-app/src/components/PrivateRoute.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import {useAuth} from "../context/auth.js";
+import { useAuth } from "../context/auth.js";
 
 function PrivateRoute({ component: Component, ...rest }) {
     const { authToken } = useAuth()
 
     return (
-        <Route {...rest} render={(props) => 
-            authToken ? (
+        <Route {...rest} render={(props) =>
+            !isNaN(authToken) ? (
                 <Component {...props} />
             ) : (
                 <Redirect to="/" />
