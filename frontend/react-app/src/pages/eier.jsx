d1ce7fa960759dbf55c08f223067df38ffab5f98
amw <ase.wilhelmsen@outlook.com>
2021-03-24 16:40:53 +0100

    Fikset mer på oversiktsvindu

--- frontend/react-app/src/pages/eier.jsx
+++ frontend/react-app/src/pages/eier.jsx
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react';
 import ReactApexChart from 'react-apexcharts';
-//import Pie from "../components/PieChart.js"
 import "./eier.css"
 
 class OwnerPage extends React.Component {
@@ -51,23 +50,38 @@ class OwnerPage extends React.Component {
 
     render() {
         return (
-            <Fragment>
-                <h1>Statistikk for eier</h1>
+            <Fragment className="fragment">
                 <div className="brukere">
-                    <p>Totalt antall brukere: {this.state.list.number_of_users}</p>
-                    <p>Antall aktive brukere i dag: {this.state.list.users_active_today}</p>
-                    <p>Antall aktive brukere denne uken: {this.state.list.users_active_week}</p>
-                    <p>Antall aktive brukere denne måneden: {this.state.list.users_active_month}</p>
+                    <div>
+                        <h1>Oversiktsvindu for eier av Røddi:</h1>
+                        <p>Totalt antall brukere:</p>
+                        <p className="antall">{this.state.list.number_of_users}</p>
+                        <p>Antall aktive brukere denne:</p>
+                    </div>
+                    <div className="aktiv">
+                        <p className="tid">Dagen</p>
+                        <p className="tall">{this.state.list.users_active_today}</p>
+                    </div>
+                    <div className="aktiv">
+                        <p className="tid">Uken</p>
+                        <p className="tall">{this.state.list.users_active_week}</p>
+                    </div>
+                    <div className="aktiv">
+                        <p className="tid">Måneden</p>
+                        <p className="tall">{this.state.list.users_active_month}</p>
+                    </div>
+                    
                 </div>
-                <div className="dodsbo">
-                    <p>Antall dødsbo: {this.state.list.number_of_estates}</p>
-                    <p>Antall uferdige dødsbo: {this.state.list.unfinished_estates}</p>
-                    <p>Gjennomsnittelig antall brukere per dødsbo: {this.state.list.users_per_estate}</p>
-                    <p>Gjennomsnittelig antall eiendeler per dødsbo: {this.state.list.assets_per_estate}</p>
-                    <p>Prosent av brukere som har brukt kommentarfunskjon: {this.state.list.commenting_users_percent}</p>
+                <div className="dodsbo_oversikt">
+                        <h3>Statistikk over dødsbo:</h3>
+                        <p>Antall dødsbo som eksisterer i applikasjonen: <b>{this.state.list.number_of_estates}</b></p>
+                        <p>Antall uferdige dødsbo på dette tidspunktet: <b>{this.state.list.unfinished_estates}</b></p>
+                        <p>Gjennomsnittelig antall brukere per dødsbo:<b> {this.state.list.users_per_estate}</b></p>
+                        <p>Gjennomsnittelig antall eiendeler per dødsbo: <b>{this.state.list.assets_per_estate}</b></p>
+                        <p>Prosent av brukere som har brukt kommentarer: <b>{this.state.list.commenting_users_percent}%</b></p>
                 </div>
+                <h3 className="stats">Statistikk over ulike relasjonstyper:</h3>
                 <div className="chart">    
-                <p>Her er en oversikt over ulike relasjoner:</p>
                     <ReactApexChart options={this.state.options} series={this.state.series} type="bar" height={300} width={700} />
                 </div>
                 
